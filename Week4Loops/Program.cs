2208ef0e22c9743fce32b12a1158fd1317352ac1
Kevin Lillepool <kevin.lillepool@tktk.ee>
2024-10-27 20:09:18 +0200

    Week 4

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Random rnd = new Random();
+
+int myRandom;
+
+
+for (int i = 0; i < 5; i++)
+{
+    myRandom = rnd.Next(1, 11);
+    Console.WriteLine($"{i + 1}. {myRandom}");
+}
+
+
+
+
+
+
+//for(int i = 0; i < 3; i++) // I++ ==> i + 1
+//{
+//    Console.WriteLine($"{i + 1}. I will not skip Zoom classes.");
+//}
